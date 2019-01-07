9085a926b496a4067b8a8f361876111911dc0d96
Max Yarmolinsky <max.yarmolinsky@gmail.com>
2019-01-07 11:18:50 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-python ./src/compare.py ./input/window.txt ./input/actual.txt ./input/predicted.txt ./output/comparison.txt
